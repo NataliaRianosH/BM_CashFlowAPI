44c45321f51cb5a5c914316e2757e993f335e4bb
NataliaRianosH <rianos.natalia@correounivalle.edu.co>
2023-11-01 00:30:49 -0500

    concepto de gasto

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.concepto_gastos),
     path('new/', views.crear_concepto_gastos, name="nuevo_concepto"),
-    path('eliminar_concepto/<int:concepto_id>/', views.eliminar_concepto_gasto, name="eliminar_concepto")
+    path('eliminar_concepto/<int:concepto_id>/', views.eliminar_concepto_gasto, name="eliminar_concepto"),
+    path('modificar_concepto/<int:concepto_id>/', views.modificar_concepto, name='modificar_concepto')
+
     
 ]
--- app/views.py
+++ app/views.py
@@ -19,3 +19,12 @@ def eliminar_concepto_gasto(request, concepto_id):
     conceptoGasto.delete()
     return redirect('/app/')
 
+def modificar_concepto(request, concepto_id):
+    if request.method == 'POST':
+        concepto = ConceptoGasto.objects.get(id=concepto_id)
+        concepto.nombre_concepto = request.POST['nombre']
+        concepto.estado = request.POST['estado']
+        concepto.descripcion = request.POST['descripcion']
+        concepto.save()
+
+    return redirect('/app/')
